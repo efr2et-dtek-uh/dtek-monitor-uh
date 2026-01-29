4c90cb6bec5ebf38dbc4d4cccc1f924f452f1a5c
efr2et-dtek-uh <efr2et+dtek-uh@gmail.com>
2026-01-30 00:18:27 +0200

    Update monitor.js
    Update

--- src/monitor.js
+++ src/monitor.js
@@ -67,6 +67,13 @@ async function getInfo() {
   }
 }
 
+function checkIsLastMessage(lastMessage) {
+  const isLastMessage = lastMessage?.message_id && lastMessage?.start_date &&
+      lastMessage?.end_date && lastMessage?.reason?.trim()
+
+  return isLastMessage
+}
+
 function checkIsOutage(info) {
   console.log("🌀 Checking power outage...")
 
@@ -89,7 +96,7 @@ function checkIsScheduled(info) {
   console.log("🌀 Checking whether power outage scheduled...")
 
   if (!info?.data) {
-    throw Error("❌ Power outage info missed.")
+    throw Error("❌ Emergency power outage info missed.")
   }
 
   const { sub_type } = info?.data?.[HOUSE] || {}
@@ -104,46 +111,107 @@ function checkIsScheduled(info) {
   return isScheduled
 }
 
-function generateMessage(info) {
-  console.log("🌀 Generating message...")
+function generateOutageMessage(info) {
+  console.log("🌀 Generating outage message...")
 
   const { sub_type, start_date, end_date } = info?.data?.[HOUSE] || {}
   const { updateTimestamp } = info || {}
 
   const reason = capitalize(sub_type)
-  const begin = start_date.split(" ")[0]
-  const end = end_date.split(" ")[0]
 
-  return [
-    "⚡️ <b>Зафіксовано відключення:</b>",
-    `🪫 <code>${begin} — ${end}</code>`,
-    "",
-    `⚠️ <i>${reason}.</i>`,
-    "\n",
-    `🔄 <i>${updateTimestamp}</i>`,
-    `💬 <i>${getCurrentTime()}</i>`,
-  ].join("\n")
+  return {
+    text: [
+      `⚡️ <b>Зафіксовано відключення:</b>`,
+      "",
+      `🪫 Час початку - ${start_date}`,
+      `🔌 Орієнтовний час відновлення - ${end_date}`,
+      "",
+      `⚠️ <i>${reason}.</i>`,
+      "",
+      `🔄 <i>Дата оновлення інформації – ${updateTimestamp}</i>`,
+      `💬 <i>Дата оновлення повідомлення – ${getCurrentTime()}</i>`
+    ].join("\n"),
+    start_date: start_date,
+    end_date: end_date,
+    reason: reason
+  }
 }
 
-async function sendNotification(message) {
+function generateCancelMessage(info, lastMessage) {
+  console.log("🌀 Generating cancel message...")
+
+  if ( checkIsLastMessage(lastMessage) ) {
+    const { updateTimestamp } = info || {}
+    const start_date = lastMessage?.start_date
+    const end_date = lastMessage?.end_date
+    const reason = capitalize(lastMessage?.reason)
+  
+    return {
+      text: [
+        `<s>⚡️ <b>Зафіксовано відключення:</b></s>`,
+        "",
+        `<s>🪫 Час початку - ${start_date}</s>`,
+        `<s>🔌 Орієнтовний час відновлення - ${end_date}</s>`,
+        "",
+        `<s>⚠️ <i>${reason}.</i></s>`,
+        "",
+        `🔄 <i>Дата оновлення інформації – ${updateTimestamp}</i>`,
+        `💬 <i>Дата оновлення повідомлення – ${getCurrentTime()}</i>`
+      ].join("\n"),
+      start_date: start_date,
+      end_date: end_date,
+      reason: reason
+    }
+  } else {
+     console.log("🔴 Missing last message.")
+  }
+}
+
+function generateBlankMessage(info) {
+  console.log("🌀 Generating blank message...")
+
+  const currentTime = getCurrentTime()
+  const { updateTimestamp } = info || {}
+  const reason = `Інформація про відключення відсутня`
+  
+  return {
+    text: [
+      `⚡️ <b>Наразі, без екстрених відключень:</b>`,
+      "",
+      `⚠️ <i>Якщо в даний момент у вас відсутнє світло, імовірно виникла аварійна ситуація. Перевірте інформацію через 15 хвилин на сайті ДТЕК.
+У разі відсутності світла у зоні, що гарантує його наявність (на графіку – білий колір), оформіть заявку на сайті ДТЕК.</i>`,
+      "",
+      `🔄 <i>Дата оновлення інформації – ${updateTimestamp}</i>`,
+      `💬 <i>Дата оновлення повідомлення – ${currentTime}</i>`
+    ].join("\n"),
+    start_date: `0`,
+    end_date: `0`,
+    reason: reason
+  }
+}
+
+async function sendNotification(message, lastMessage) {
   if (!TELEGRAM_BOT_TOKEN)
     throw Error("❌ Missing telegram bot token or chat id.")
   if (!TELEGRAM_CHAT_ID) throw Error("❌ Missing telegram chat id.")
 
   console.log("🌀 Sending notification...")
 
-  const lastMessage = loadLastMessage() || {}
   try {
     const response = await fetch(
       `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/${
-        lastMessage.message_id ? "editMessageText" : "sendMessage"
+        lastMessage?.message_id &&
+        message?.start_date === lastMessage?.start_date &&
+        message?.end_date === lastMessage?.end_date &&
+        message?.reason?.trim() === lastMessage?.reason?.trim()
+        ? "editMessageText" : "sendMessage"        
       }`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           chat_id: TELEGRAM_CHAT_ID,
-          text: message,
+          text: message?.text ?? undefined,
           parse_mode: "HTML",
           message_id: lastMessage.message_id ?? undefined,
         }),
@@ -151,25 +219,100 @@ async function sendNotification(message) {
     )
 
     const data = await response.json()
-    saveLastMessage(data.result)
-
-    console.log("🟢 Notification sent.")
+      
+    if (!response.ok) {
+      if ( data?.error_code === 400 && data?.description?.includes("message is not modified")) {
+        console.log("ℹ️ Telegram: message not modified, skipping")
+        return
+      }
+      console.error("🪲 Telegram API error:", JSON.stringify(data, null, 2));
+    } else {
+      saveLastMessage({ ...data.result, ...message})
+      console.log("🟢 Notification sent.")
+      //console.log("🪲 Telegram API response:", JSON.stringify(data, null, 2));
+    } 
   } catch (error) {
     console.log("🔴 Notification not sent.", error.message)
-    deleteLastMessage()
+    saveLastMessage()
   }
+}
+
+async function sendCancelReplay(lastMessage) {
+  if (!TELEGRAM_BOT_TOKEN)
+    throw Error("❌ Missing telegram bot token or chat id.")
+  if (!TELEGRAM_CHAT_ID) throw Error("❌ Missing telegram chat id.")
+
+  if ( checkIsLastMessage(lastMessage) ) {
+    console.log("🌀 Sending cancel replay...")
+    
+    try {
+      const response = await fetch(
+        `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            chat_id: TELEGRAM_CHAT_ID,
+            text: [
+              `🔌 <b>Наразі, без екстрених відключень:</b>`,
+              "",
+            `⚠️ <i>Якщо в даний момент у вас відсутнє світло, імовірно виникла аварійна ситуація. Перевірте інформацію через 15 хвилин на сайті ДТЕК.
+У разі відсутності світла у зоні, що гарантує його наявність (на графіку – білий колір), оформіть заявку на сайті ДТЕК.</i>`
+            ].join("\n"),
+            parse_mode: "HTML",
+            reply_to_message_id: lastMessage.message_id,
+          }),
+        }
+      )
+  
+      console.log("🟢 Cancel replay sent.")
+    } catch (error) {
+      console.log("🔴 Cancel replay not sent.", error.message) 
+    }
+  } else {
+      console.log("🌀 Missing notification message.")
+  }
+  saveLastMessage()
+}
+
+async function clearTGQueue() {
+  if (!TELEGRAM_BOT_TOKEN)
+    throw Error("❌ Missing telegram bot token or chat id.")
+  if (!TELEGRAM_CHAT_ID) throw Error("❌ Missing telegram chat id.")
+  
+  const res = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getUpdates`);
+  const data = await res.json();
+
+  if (!data.ok || data.result.length === 0) {
+    console.log("🟢 TG Queue empty");
+    return;
+  }
+
+  const lastId = data.result[data.result.length - 1].update_id;
+  await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getUpdates?offset=${lastId + 1}`);
+
+  console.log("🟢 Clear TG Queue to update_id:", lastId);
 }
 
 async function run() {
   const info = await getInfo()
+  const lastMessage = loadLastMessage() || {}
+  
   const isOutage = checkIsOutage(info)
-
-  if (!isOutage) return
-
   const isScheduled = checkIsScheduled(info)
-  if (isOutage && !isScheduled) {
-    const message = generateMessage(info)
-    await sendNotification(message)
+  let message
+
+  if (isOutage) {
+    message = generateOutageMessage(info)
+    await clearTGQueue()
+    await sendNotification(message, lastMessage)
+  } else {
+    if (checkIsLastMessage(lastMessage) && !isOutage) {
+      message = generateCancelMessage(info, lastMessage)
+      await clearTGQueue()
+      await sendNotification(message, lastMessage)
+      await sendCancelReplay(lastMessage)
+    }
   }
 }
 
